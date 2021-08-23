e670c0ff300b77680951a47bf648a188d081792f
Lars P <l.pickelin@gmail.com>
2021-08-23 18:27:31 +0200

    Use java native

---
+++ src/main/java/com/endstation/visitor/api/EndstationVisitorApiApplication.java
@@ -0,0 +1,13 @@
+package com.endstation.visitor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndstationVisitorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndstationVisitorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endstation/visitor/api/EndstationVisitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endstation.visitor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndstationVisitorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
